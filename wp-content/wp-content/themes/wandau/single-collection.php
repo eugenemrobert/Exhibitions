996765801ec1de55faa6a0ad7200ec536887e1fe
eugenemrobert <eugenemuloma40@gmail.com>
2021-09-16 14:40:17 +0300

    Removed media files

--- wp-content/wp-content/themes/wandau/single-collection.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); ?>
-<?php wandau_render_page_header( 'page' ); ?>
-<?php
-while ( have_posts() ):
-  the_post();
-?>
-<?php the_content(); ?>
-<?php endwhile; ?>
-<?php get_footer(); ?>
